33c05485bc575765ee90afca77e458a490d94713
Alessandro Fragnani <alefragnani@hotmail.com>
2017-03-14 12:42:08 -0300

    code coverage working

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,10 @@ var gulp  = require('gulp'),
     cp = require('child_process'),
     pp = require('process');
 
-// create a default task and just log a message
-// gulp.task('default', function() {
-//   return gutil.log('Gulp is running!')
-// });
+// the default task is.....
 gulp.task('default', ["create-folders"]);
 
-// create folders
+// create additional folders, used by the building process
 gulp.task("create-folders", function() {
   if (!fs.existsSync(path.join(__dirname, 'Artifacts'))) {
     fs.mkdirSync(path.join(__dirname, 'Artifacts'));
@@ -20,95 +17,27 @@ gulp.task("create-folders", function() {
   if (!fs.existsSync(path.join(__dirname, 'Bin'))) {
     fs.mkdirSync(path.join(__dirname, 'Bin'));
   }
+  if (!fs.existsSync(path.join(__dirname, 'Bin\\Coverage'))) {
+    fs.mkdirSync(path.join(__dirname, 'Bin\\Coverage'));
+  }
   if (!fs.existsSync(path.join(__dirname, 'Lib'))) {
     fs.mkdirSync(path.join(__dirname, 'Lib'));
   }
   return gutil.log('CREATE-FOLDERS');
 });
 
-// compile
+// compile the main project
 gulp.task("compile", ["create-folders"], function() {
   gutil.log('compile-entrando-chdir');
   gutil.log(__dirname);
   pp.chdir(path.join(__dirname, "src"));
   gutil.log('compile-entrando-spawn');
   var bb = cp.spawnSync(path.join(__dirname, 'src\\dcc.bat'));
-  //gutil.log(bb);
   gutil.log(bb.output.toString());
-  // bb.stdout.on('data', function (data) {
-  //   gutil.log(data.toString());
-  // });
-  // bb.stderr.on('data', function (data) {
-  //   gutil.log('ERRO: ' + data.toString());
-  // });
-  // bb.on('exit', function (code) {
-  //   console.log('child process exited with code ' + code.toString());
-  //   pp.chdir("..\\");
   return gutil.log('COMPILE exit');
-  });
-// // compile
-// gulp.task("compile", ["create-folders"], function() {
-//   gutil.log('compile-entrando-chdir');
-//   gutil.log(__dirname);
-//   pp.chdir(path.join(__dirname, "src"));
-//   gutil.log('compile-entrando-spawn');
-//   var bb = cp.spawn(path.join(__dirname, 'src\\dcc.bat'));
-//   bb.stdout.on('data', function (data) {
-//     gutil.log(data.toString());
-//   });
-//   bb.stderr.on('data', function (data) {
-//     gutil.log('ERRO: ' + data.toString());
-//   });
-//   bb.on('exit', function (code) {
-//     console.log('child process exited with code ' + code.toString());
-//     pp.chdir("..\\");
-//     return gutil.log('COMPILE exit');
-//   });
+});
 
-
-
-
-  ////////////////////////////////
-
-  // var bb = cp.spawn('D:\\Program Files\\Embarcadero\\Studio\\17.0\\bin\\DCC32.EXE', ['Project1.dpr', '-B   -NUD:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\Lib  -UD:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\src']);
-
-  //var bb = cp.spawn('D:\\Program Files\\Embarcadero\\Studio\\17.0\\bin\\DCC32.EXE', ['Project1.dpr -B -E..\Bin -NU..\Lib -GD']);
-
-  // cp.execFile('D:\\Program Files\\Embarcadero\\Studio\\17.0\\bin\\DCC32.EXE', ['Project1.dpr', '-B -E..\Bin -NU..\Lib -GD'], (err, stdout, stderr) => {
-
-  //       if (err) {
-  //         console.log('Erro ERR: ' + err);
-  //         return 'Error';
-  //       }
-
-  //       if (stderr.toString() != '') {
-  //         console.log('Erro STDERR: ' + stderr);
-  //         return 'Error: ' + stderr.toString();
-  //       }
-
-  //       console.log(stdout.toString());
-  //     });
-  
-
-  // "D:\Program Files\Embarcadero\Studio\17.0\Bin\dcc32.exe" "D:\_Fragnani\_Apps\GitHub\delphi-squad-demo\src\Project1.dpr" -b d:\_Fragnani\_Apps\GitHub\delphi-squad-demo\src\Project1.dpr -E..\Bin -NU..\Lib -GD
-  
-  // var bb = cp.spawn('D:\\Program Files\\Embarcadero\\Studio\\17.0\\bin\\DCC32.EXE', ['Project1.dpr', '-B   -NUD:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\Lib -LED:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\Bin  -LND:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\Bin -ED:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\Bin -UD:\\_Fragnani\\_Apps\\GitHub\\delphi-squad-demo\\src -GD']);
-  
-  // output spawn
-  // bb.stdout.on('data', function (data) {
-  //   gutil.log(data.toString());
-  // });
-  // bb.stderr.on('data', function (data) {
-  //   gutil.log('ERRO: ' + data.toString());
-  // });
-  // bb.on('exit', function (code) {
-  //   console.log('child process exited with code ' + code.toString());
-  //   pp.chdir("..\\");
-  //   return gutil.log('COMPILE');
-  // });
-
-//});
-
+// compile the test project
 gulp.task("compile-tests", ["compile"], function() {
   gutil.log('compile-tests-entrando-chdir');
   gutil.log(__dirname);
@@ -116,33 +45,8 @@ gulp.task("compile-tests", ["compile"], function() {
   gutil.log('compile-tests-entrando-spawn');
   var bb = cp.spawnSync(path.join(__dirname, 'tests\\dcc.bat'));
   gutil.log(bb.output.toString());
-  // bb.stdout.on('data', function (data) {
-  //   gutil.log(data.toString());
-  // });
-  // bb.stderr.on('data', function (data) {
-  //   gutil.log('ERRO: ' + data.toString());
-  // });
-  // bb.on('exit', function (code) {
-  //   console.log('child process exited with code ' + code.toString());
-  //   pp.chdir("..\\");
-    return gutil.log('COMPILE-TESTS exit');
-//  });
+  return gutil.log('COMPILE-TESTS exit');
 });
-// // compile
-// gulp.task("compile", ["create-folders"], function() {
-//   var bb = cp.spawn('D:\\Program Files\\Embarcadero\\Studio\\17.0\\bin\\DCC32.EXE', ['src\\Project1.dpr']);
-//   bb.stdout.on('data', function (data) {
-//     gutil.log(data.toString());
-//   });
-//   bb.stderr.on('data', function (data) {
-//     gutil.log('ERRO: ' + data.toString());
-//   });
-//   bb.on('exit', function (code) {
-//     console.log('child process exited with code ' + code.toString());
-//     return gutil.log('COMPILE');
-//   });
-
-// });
 
 // run unit tests
 gulp.task("unit-test", ["compile-tests"], function() {
@@ -152,26 +56,29 @@ gulp.task("unit-test", ["compile-tests"], function() {
   gutil.log('unit-test-spawn');
   var bb = cp.spawnSync(path.join(__dirname, 'Bin\\Project1Tests.exe'));
   gutil.log(bb.output.toString());
-  // bb.stdout.on('data', function (data) {
-  //   gutil.log(data.toString());
-  // });
-  // bb.stderr.on('data', function (data) {
-  //   gutil.log('ERRO: ' + data.toString());
-  // });
-  // bb.on('exit', function (code) {
-  //   console.log('child process exited with code ' + code.toString());
-  //   pp.chdir("..\\");
-    return gutil.log('UNIT-TEST exit');
-//  });
+  return gutil.log('UNIT-TEST exit');
 });
+
+function create_cc_bat(projectName, unitName) {
+  var ccexe = path.join(__dirname, "Coverage\\CodeCoverage.exe")
+  var cccomand = path.join(__dirname, "Coverage\\CodeCoverage.exe") + " -e " + path.join(__dirname, "Bin",  projectName + ".exe") + " -m " + path.join(__dirname, "Bin", projectName + ".map") + " -sd " + path.join(__dirname, "src") + " -u " + unitName + " -html -emma -xml -od " + path.join(__dirname, "Bin", "Coverage");
+  fs.writeFileSync(path.join(__dirname, 'Coverage\\ccX.bat'), cccomand);
+}
 
 // run code coverage
 gulp.task("code-coverage", function() {
-  return gutil.log('code-coverage');
+  gutil.log('code-coverage');
+  gutil.log(__dirname);
+  // pp.chdir(path.join(__dirname, "Bin"));
+  // gutil.log('code-coverage-spawn');
+  create_cc_bat('Project1Tests', 'uCalculadora.pas');
+  var bb = cp.spawnSync(path.join(__dirname, 'Coverage\\ccX.bat'));
+  gutil.log(bb.output.toString());
+  return gutil.log('code-coverage exit');
 });
 
 
-// zip
+// zip the artifacts
 gulp.task("zip", function() {
   return gutil.log('ZIP');
 });
